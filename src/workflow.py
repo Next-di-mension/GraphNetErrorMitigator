dc85d91ef19d408cba3a9ebe8a27cfb9e0b848cc
Next-di-mension <srushtipatil926@gmail.com>
2024-03-14 12:06:36 +0530

    latest

--- src/workflow.py
+++ src/workflow.py
@@ -4,8 +4,9 @@ def run_script(script_name):
     subprocess.run(["python", script_name], check=True)
 
 def main():
-    run_script("src/qiskit_noise.py")
-    run_script("src/gnn_regressor.py")
+    run_script("src/training_data_generation.py")
+    run_script("src/test_data_generation.py")
+    run_script("src/train.py")
 
 if __name__ == "__main__":
     main()
